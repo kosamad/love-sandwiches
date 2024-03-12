ada9c069d33a3b91bfc7148e0e4769eb0058a0c8
kosamad samad <KarenLSamad@gmail.com>
2024-03-12 10:51:22 +0000

    Create get_sales_data function to get data string from user.

--- run.py
+++ run.py
@@ -15,6 +15,26 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches') # access love sandwiches sheet us
 
 sales = SHEET.worksheet('sales') # corresponds to name ('sales' of the worksheet in love_sandwiches sheet.
 
-data = sales.get_all_values() # pulss values from sales worksheet
+# data = sales.get_all_values() # pulls values from sales worksheet (checks if API working, not requird once done)
+# print(data)
 
-print(data)
+# note normally sales data would be collected with an API between Python program and love sandwiches uers but for this project, a user is required to imput data into the terminal 
+
+#csv = comma-separated values, a file type to give table structured format
+# first function to collect sales data from user
+
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print('Please enter sales data from the last market.') # instructions for user
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n") #\n puts a space between lines
+
+    data_str = input('Enter your data here:') # where user inputs data ( output a string)
+    print(f"The data provided is {data_str}") # checks values are assinged
+
+get_sales_data() # call function
+
+
+
